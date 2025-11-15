120f92bb751611ee6b0762e853e75a379e74764b
Andrei Samoila <samoilaandrei76@gmail.com>
2025-11-15 13:55:02 +0200

    Invoice template

---
+++ EasyBilling.Application/Services/InvoiceService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyBilling.Application.Services
+{
+    public class InvoiceService
+    {
+        public void GenerateInvoice()
+        {
+            // Implementation for creating an invoice goes here.
+        }
+    }
+}
---
+++ EasyBilling.Presentation/Controllers/V1/Invoice.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyBilling.Presentation.Controllers.V1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Invoice : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult GenerateInvoice()
+        {
+            // Implementation for creating an invoice goes here.
+            return Ok("Invoice created successfully.");
+        }
+
+        [HttpGet]
+        public IActionResult GetInvoice()
+        {
+            return Ok("Hello world");
+        }
+    }
+}
