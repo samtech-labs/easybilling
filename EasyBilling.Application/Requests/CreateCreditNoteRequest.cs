4a4f501a73d6dc5df63ad729c28bff01f48d2302
Andrei Samoila <samoilaandrei76@gmail.com>
2026-01-09 16:57:42 +0200

    Credit Notes (#46)

---
+++ EasyBilling.Application/Requests/CreateCreditNoteRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace EasyBilling.Application.Requests
+{
+    public class CreateCreditNoteRequest
+    {
+        public Guid OriginalInvoiceId { get; set; }
+        public string? Series { get; set; }
+        public string? Number { get; set; }
+
+        public List<CreditNoteLinesRequest>? Lines { get; set; }
+    }
+}
---
+++ EasyBilling.Domain/Enums/InvoiceType.cs
@@ -0,0 +1,7 @@
+﻿namespace EasyBilling.Domain.Enums;
+
+public enum InvoiceType
+{
+    Invoice = 380,
+    CreditNote = 381
+}
